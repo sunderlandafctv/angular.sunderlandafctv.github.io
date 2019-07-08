2fb16d19011706878a14b374867b7ba5f6955e4f
Ryan Comerford <ryncmrfrd@outlook.com>
2019-07-08 12:02:04 +1000

    fixed player page video errors and started seasson pages

--- src/app/season/season-routing.module.ts
+++ src/app/season/season-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { NavComponent } from './nav/nav.component'
+import { HomeComponent } from './home/home.component'
+import { VideosComponent } from './videos/videos.component'
+import { PlayersComponent } from './players/players.component'
+
+const routes: Routes = [
+  { 
+    path: "", 
+    redirectTo: "home", 
+    pathMatch: "prefix" 
+  },
+  //nav bar
+  { 
+    path: "", 
+    component: NavComponent, 
+    outlet: "nav" 
+  },
+  //actual pages
+  { 
+    path: "home", 
+    component: HomeComponent 
+  },
+  { 
+    path: "videos", 
+    component: VideosComponent 
+  },
+  { 
+    path: "players", 
+    component: PlayersComponent 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/season/season.module.ts
+++ src/app/season/season.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SeasonRoutingModule } from './season-routing.module';
+import { HomeComponent } from './home/home.component';
+import { VideosComponent } from './videos/videos.component';
+import { PlayersComponent } from './players/players.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HomeComponent, VideosComponent, PlayersComponent, NavComponent],
   imports: [
     CommonModule,
     SeasonRoutingModule
